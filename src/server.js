e0b70eb0b570368e8066041a91c69f6b43341627
dawwson <shines1427@gmail.com>
2024-03-12 08:33:14 +0900

    Feat: 서버 작성

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log("hello nodejs");
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port http://localhost:${PORT} 🚀`);
+});
