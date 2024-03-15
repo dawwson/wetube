bd53d2c22a1edcc01c8db2759e4773286a254a4f
dawwson <shines1427@gmail.com>
2024-03-15 15:22:47 +0900

    Add: morgan logger 추가

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
+const loggerMiddleware = morgan("devs");
 
 const app = express();
+app.use(loggerMiddleware);
+
+app.get("/", (req, res, next) => {
+  return res.end();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost:${PORT} 🚀`);
