af62033bd49b0ea4017780403a2f0b01ea08e9ef
dawwson <shines1427@gmail.com>
2024-03-15 15:59:28 +0900

    Refactor: router 파일로 분리

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const videoRouter = express.Router();
+
+const handleVideoWatch = (req, res) => {
+  res.send("Watch Video");
+};
+
+videoRouter.get("/watch", handleVideoWatch);
+
+export default videoRouter;
