83ca4baea128a1d9f8275968fe12edf1d739f79e
dawwson <shines1427@gmail.com>
2024-03-15 15:44:10 +0900

    Feat: 라우터 작성

--- src/server.js
+++ src/server.js
@@ -2,14 +2,36 @@ import express from "express";
 import morgan from "morgan";
 
 const PORT = 4000;
-const loggerMiddleware = morgan("devs");
-
 const app = express();
-app.use(loggerMiddleware);
 
-app.get("/", (req, res, next) => {
-  return res.end();
-});
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => {
+  res.send("Home");
+};
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => {
+  res.send("Edit User");
+};
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleVideoWatch = (req, res) => {
+  res.send("Watch Video");
+};
+
+videoRouter.get("/watch", handleVideoWatch);
+
+app.use(morgan("dev"));
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost:${PORT} 🚀`);
