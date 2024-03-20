109a95168cfc790b785639bc00be13cd6d862659
dawwson <shines1427@gmail.com>
2024-03-21 01:52:03 +0900

    Feat: form 요청으로 받은 값으로 데이터 수정

--- src/apis/videoApiController.js
+++ src/apis/videoApiController.js
@@ -1,4 +1,10 @@
+import { videos } from "../../data";
+
 export const editVideo = (req, res) => {
   const { id } = req.params;
+  const { title } = req.body;
+
+  videos[id - 1].title = title;
+
   res.redirect(`/videos/${id}`);
 };
