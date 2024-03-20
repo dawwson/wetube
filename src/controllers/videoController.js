83c217a0d5d571cd708ebd96eb590d1e9117d500
dawwson <shines1427@gmail.com>
2024-03-21 01:51:07 +0900

    Fix: mock 데이터 파일로 분리

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,30 +1,4 @@
-const user = { username: "Nico", loggedIn: true };
-const videos = [
-  {
-    title: "Video #1",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 10,
-    id: 1,
-  },
-  {
-    title: "Video #2",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 10,
-    id: 2,
-  },
-  {
-    title: "Video #3",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 10,
-    id: 3,
-  },
-];
+import { user, videos } from "../../data";
 
 export const trending = (req, res) => {
   res.render("home", { pageTitle: "Home", user, videos });
