de8ca92f1bf6b1a95db836452e1d7bc4451334e1
dawwson <shines1427@gmail.com>
2024-03-21 00:48:43 +0900

    Feat: mixin으로 중복코드 제거

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,33 @@
 const fakeUser = { username: "Nico", loggedIn: true };
 
 export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home", fakeUser });
+  const videos = [
+    {
+      title: "Video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 10,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 10,
+      id: 2,
+    },
+    {
+      title: "Video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 10,
+      id: 3,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
 
 export const searchVideo = (req, res) => {
