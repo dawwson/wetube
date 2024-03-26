9c9d807f392a7542886fe51823bce50b6ed5671f
dawwson <shines1427@gmail.com>
2024-03-26 18:30:33 +0900

    Fix: 데이터베이스 연결 uri 수정

--- src/db.js
+++ src/db.js
@@ -1,6 +1,19 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+dotenv.config();
+
+const dbName = process.env.DB_DATABASE;
+const dbPort = process.env.DB_PORT;
+const dbUser = process.env.DB_BACKEND_USERNAME;
+const dbPassword = process.env.DB_BACKEND_PASSWORD;
+
+// NOTE: authSource는 사용자 인증정보 collection(= users)를 가진 데이터베이스 이름을 지정합니다.
+const mongoUri = `mongodb://${dbUser}:${dbPassword}@127.0.0.1:${dbPort}/${dbName}?authSource=admin`;
+
+mongoose.connect(mongoUri, {
+  autoCreate: true, // 모든 모델에 대한 콜렉션 자동 생성
+});
 
 const db = mongoose.connection;
 
@@ -9,5 +22,5 @@ db.on("error", (error) => {
 });
 
 db.once("open", () => {
-  console.log("✅ Connected to DB");
+  console.log("✅ Connected to DB 🎉");
 });
