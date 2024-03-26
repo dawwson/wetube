69f976c721a6b70e2a87d435c3e891f620fcdbc1
dawwson <shines1427@gmail.com>
2024-03-26 15:31:31 +0900

    Fix: mock 데이터 삭제

--- src/apis/videoApiController.js
+++ src/apis/videoApiController.js
@@ -1,24 +1,10 @@
-import { videos } from "../../data";
-
 export const editVideo = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-
-  videos[id - 1].title = title;
 
   res.redirect(`/videos/${id}`);
 };
 
 export const uploadVideo = (req, res) => {
-  const { title } = req.body;
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   res.redirect("/");
 };
