94f0a8fdf418192328e40429d35a4a2b25bb65ac
dawwson <shines1427@gmail.com>
2024-03-28 18:15:04 +0900

    Add: 인증정보 검증 로직 추가

--- src/apis/userApiController.js
+++ src/apis/userApiController.js
@@ -1,7 +1,32 @@
 import User from "../models/User";
 
-export const createUser = (req, res) => {
-  const { email, username, password, name, location } = req.body;
+export const createUser = async (req, res) => {
+  const { email, username, password, confirmPassword, name, location } =
+    req.body;
+
+  if (password !== confirmPassword) {
+    return res.render("pages/join", {
+      pageTitle: "Join",
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+
+  const isUsernameExists = await User.exists({ username });
+  if (isUsernameExists) {
+    return res.render("pages/join", {
+      pageTitle: "Join",
+      errorMessage: "This username is already taken.",
+    });
+  }
+
+  const isEmailExists = await User.exists({ email });
+  if (isEmailExists) {
+    return res.render("pages/join", {
+      pageTitle: "Join",
+      errorMessage: "This email is already taken.",
+    });
+  }
+
   User.create({
     email,
     username,
