58a716705bf280803fdaf7fd5943ad66eb0ae205
dawwson <shines1427@gmail.com>
2024-04-03 16:36:26 +0900

    Refactor: github app client id 환경변수로 관리

--- src/config.js
+++ src/config.js
@@ -6,6 +6,7 @@ const config = {
   port: Number(process.env.PORT),
   cookieSecret: process.env.COOKIE_SECRET,
   mongoUrl: process.env.MONGO_URL,
+  githubClientId: process.env.GITHUB_CLIENT_ID,
 };
 
 export default config;
