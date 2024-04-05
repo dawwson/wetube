86c4900b24af3c139ffccf0b534b1687b0576711
dawwson <shines1427@gmail.com>
2024-04-05 22:19:04 +0900

    Fix: 패스워드 수정할 때만 해싱되도록 save pre hook 수정

--- src/models/User.js
+++ src/models/User.js
@@ -38,10 +38,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  if (this.socialOnly) {
-    return;
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
   }
-  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userSchema);
