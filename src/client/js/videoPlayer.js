6bc4d17d38e4893f8cd03e33beb5a7c21c994624
dawwson <shines1427@gmail.com>
2024-04-06 04:23:00 +0900

    Feat: 볼륨 조절 기능 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,9 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = () => {
   if (video.paused) {
     video.play();
@@ -20,7 +23,7 @@ const handleMute = () => {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
 const handlePause = () => {
@@ -31,5 +34,16 @@ const handlePlay = () => {
   playBtn.innerText = "Pause";
 };
 
+const handleVolumeChange = (event) => {
+  const value = event.target.value;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange); // NOTE: input => 실시간 range 감지
