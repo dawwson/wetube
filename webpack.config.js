e891390e367143e0c48c943a27cbffbf379ecbd2
dawwson <shines1427@gmail.com>
2024-04-06 01:43:33 +0900

    Add: loader 변경하여 별도의 css 파일 추출하도록 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
+const MiniCssExtractrPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   // 처리하고 싶은 파일의 경로
   entry: "./src/client/js/main.js",
   mode: "development",
+  plugins: [
+    new MiniCssExtractrPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
     // 변환 결과물 파일 이름
-    filename: "main.js",
+    filename: "js/main.js",
     // 변환 결과물 경로
-    path: path.resolve(__dirname, "assets", "js"),
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [
@@ -23,7 +29,11 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [
+          MiniCssExtractrPlugin.loader, // css를 추출해서 별도의 파일로 만들어줌
+          "css-loader", //
+          "sass-loader", // scss를 css로 변환
+        ],
       },
     ],
   },
