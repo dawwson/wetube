f806a9cd1f958afb8dfc119aef2d96a947e8594c
dawwson <shines1427@gmail.com>
2024-04-08 20:04:52 +0900

    Add: control에 스타일 적용 및 시간 단위 수정

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,58 +1,37 @@
+// TODO: 스페이스 바 & 비디오 클릭으로 play & stop 구현
 const videoContainer = document.getElementById("videoContainer");
 const video = document.querySelector("video");
 const videoControls = document.getElementById("videoControls");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volume");
-const cuttentTime = document.getElementById("cuttentTime");
+const cuttentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenBtnIcon = fullScreenBtn.querySelector("i");
 
 let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
 /**
- * video가 멈춘 상태라면 video를 재생,
- * video가 재생되고 있는 상태라면 video를 일시정지
+ * video에서 마우스를 움직이면 기존 timeout 취소하고 새로운 timeout 생성,
+ * video에서 마우스를 멈추면 3초 후에 video control 사라짐
  */
-const handlePlayAndStop = () => {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
+const handleMouseMove = () => {
+  if (controlsMovementTimeout) {
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
   }
-  playBtn.innerText = video.paused ? "Play" : "Pause";
+  videoControls.classList.add("showing");
+  controlsMovementTimeout = setTimeout(hideControls, 3000);
 };
 
-/**
- * video가 음소거 상태라면 이전에 설정한 볼륨으로 설정
- * video가 음소거 상태가 아니라면 음소거 상태로 설정
- */
-const handleMuteAndUnmute = () => {
-  if (video.muted) {
-    video.muted = false;
-  } else {
-    video.muted = true;
-  }
-  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : volumeValue;
-};
-
-/**
- * volumeRange의 value가 바뀔 때 마다 해당 value로 video의 volume 설정
- * @param {*} event
- */
-const handleVolumeChange = (event) => {
-  const value = event.target.value;
-
-  if (video.muted) {
-    video.muted = false;
-    muteBtn.innerText = "Mute";
-  }
-  volumeValue = value;
-  video.volume = value;
+const hideControls = () => {
+  videoControls.classList.remove("showing");
 };
 
 /**
@@ -73,24 +52,24 @@ const handleTimeUpdate = () => {
 };
 
 const formatTime = (seconds) => {
-  return new Date(seconds * 1000).toISOString().substring(11, 19);
+  // NOTE: 1시간 넘어가면 hh:mm:ss, 1시간 미만이면 mm:ss
+  const formatted = new Date(seconds * 1000).toISOString().substring(11, 19);
+  return formatted.substring(0, 2) === "00"
+    ? formatted.substring(3)
+    : formatted;
 };
 
 /**
- * video에서 마우스를 움직이면 기존 timeout 취소하고 새로운 timeout 생성,
- * video에서 마우스를 멈추면 3초 후에 video control 사라짐
+ * video가 멈춘 상태라면 video를 재생,
+ * video가 재생되고 있는 상태라면 video를 일시정지
  */
-const handleMouseMove = () => {
-  if (controlsMovementTimeout) {
-    clearTimeout(controlsMovementTimeout);
-    controlsMovementTimeout = null;
+const handlePlayAndStop = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
   }
-  videoControls.classList.add("showing");
-  controlsMovementTimeout = setTimeout(hideControls, 3000);
-};
-
-const hideControls = () => {
-  videoControls.classList.remove("showing");
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 /**
@@ -103,6 +82,37 @@ const handleTimelineChange = (event) => {
 };
 
 /**
+ * volumeRange의 value가 바뀔 때 마다 해당 value로 video의 volume 설정
+ * @param {*} event
+ */
+const handleVolumeChange = (event) => {
+  const value = event.target.value;
+
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
+/**
+ * video가 음소거 상태라면 이전에 설정한 볼륨으로 설정
+ * video가 음소거 상태가 아니라면 음소거 상태로 설정
+ */
+const handleMuteAndUnmute = () => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+/**
  * 전체 화면인 상태이면 전체 화면 상태를 멈춤
  * 전체 화면이 아닌 상태라면 video와 controls를 전체 화면으로 전환
  */
@@ -111,18 +121,18 @@ const handleFullScreen = () => {
 
   if (fullScreen) {
     document.exitFullscreen();
-    fullScreenBtn.innerText = "Enter Full Screen";
+    fullScreenBtnIcon.classList = "fas fa-expand";
   } else {
     videoContainer.requestFullscreen();
-    fullScreenBtn.innerText = "Exit Full Screen";
+    fullScreenBtnIcon.classList = "fas fa-compress";
   }
 };
 
-playBtn.addEventListener("click", handlePlayAndStop);
-muteBtn.addEventListener("click", handleMuteAndUnmute);
-volumeRange.addEventListener("input", handleVolumeChange);
+videoContainer.addEventListener("mousemove", handleMouseMove);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
-video.addEventListener("mousemove", handleMouseMove);
+playBtn.addEventListener("click", handlePlayAndStop);
 timeline.addEventListener("input", handleTimelineChange);
+volumeRange.addEventListener("input", handleVolumeChange);
+muteBtn.addEventListener("click", handleMuteAndUnmute);
 fullScreenBtn.addEventListener("click", handleFullScreen);
